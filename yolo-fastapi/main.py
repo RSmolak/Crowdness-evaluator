16c9244b2b867f75be6680891f83f4bda3eeb5f0
RSmolak <rafal.smolak@gmail.com>
2022-05-24 11:42:40 +0200

    fastapi add

---
+++ yolo-fastapi/main.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI, File
+from segmentation import get_yolov5, get_image_from_bytes
+from starlette.responses import Response
+import io
+from PIL import Image
+import json
+from fastapi.middleware.cors import CORSMiddleware
+
+model = get_yolov5()
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/img-to-json")
+async def detect_people_return_json_result(file: bytes = File(...)):
+    input_image = get_image_from_bytes(file)
+    results = model(input_image)
+    detect_res = results.pandas().xyxy[0].to_json(orient="records")
+    detect_res = json.loads(detect_res)
+    return {"result": detect_res}
+
+
+@app.post("/img-to-img")
+async def detect_people_return_base64_img(file: bytes = File(...)):
+    input_image = get_image_from_bytes(file)
+    results = model(input_image)
+    results.render()
+    for img in results.imgs:
+        bytes_io = io.BytesIO()
+        img_base64 = Image.fromarray(img)
+        img_base64.save(bytes_io, format="jpeg")
+    return Response(content=bytes_io.getvalue(), media_type="image/jpeg")
+
+
+@app.post("/img-to-number")
+async def detect_people_return_number(file: bytes = File(...)):
+    input_image = get_image_from_bytes(file)
+    results = model(input_image)
+    detect_res = results.pandas().xyxy[0].to_json(orient="records")
+    detect_res = json.loads(detect_res)
+    number = str(len(detect_res))
+    return {number}
